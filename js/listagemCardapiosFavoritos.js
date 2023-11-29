14c8cf517ccbe38123cdd9356a0555681fc3c7f6
Karine Piacentini Coelho da Costa <karinepcc@gmail.com>
2023-11-29 00:22:05 -0300

    Adição ícones de upload e remoção de cardápio e comportamento do favoritar na listagem de cardápios

---
+++ js/listagemCardapiosFavoritos.js
@@ -0,0 +1,12 @@
+
+function toggleFavorito(heartId) {
+    if(document.getElementById(heartId).classList.contains('fa-solid')) {
+        document.getElementById(heartId).classList.replace('fa-solid', 'fa-regular');
+    } else {
+        document.getElementById(heartId).classList.replace('fa-regular', 'fa-solid');
+    }
+}
+
+function deleteMenu(id) {
+    alert("Tem certeza que gostaria de remover o cardápio? \n A operação não pode ser desfeita.");
+}
